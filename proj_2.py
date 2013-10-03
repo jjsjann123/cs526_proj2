41e963b3df9c85c0f2ee72942630d4843c851c07
alex <alex.jann2012@gmail.com>
2013-10-02 19:10:03 -0500

    initialized

---
+++ proj_2.py
@@ -0,0 +1,21 @@
+
+
+class PlanetarySystem:
+	starList = {}
+	planetList = []
+	
+	def __addStar(str):
+		print str
+	
+	def __addPlanet(str):
+		print str
+	
+	def __drawSystem():
+	
+def readFromFile(planetarySystem, filename, dir = None):
+	if ( dir != None	)
+		filename = dir + filename
+	xmldoc = minidom.parse(filename)
+	
+	xmldoc.getElementsByTagName
+	
