1b30a3a2bd9cd53b0ca55300d27db88864bda29e
j_ <alex.jann2012@gmail.com>
2013-10-09 02:11:29 -0500

    Trying to work on multiples. Moving to Shader!

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 from proj_2 import *
+from omegaToolkit import *
+
+
+def drawContainer(self, column, row, container):
+	print "test"
 
 dataDir = './data/'
 caveSystem = None
@@ -7,21 +12,35 @@ caveSystem = None
 list = readPlanetarySystemFile('starList', dataDir)
 readPlanetsFile(list, 'planetsList_tmp_earth.csv', dataDir)
 
-drawPlanetarySystems(list)
-#	draw planetary sytem:
-
 cam = getDefaultCamera()
 cam.setPosition( Vector3( 0, 0, 5))
 sphere = SphereShape.create( 0.1, 4 )
 
-caveSystem = list['Sun']
+q = list['Sun']
 
-def onUpdate(frame, t, dt):
-	global caveSystem
-	planetList = caveSystem.planetObjList
-	if caveSystem != None:
-		for name in planetList:
-			caveSystem.planetRotate(dt * caveSystem.speedScale /planetList[name][2], name)
-			planetList[name][1].yaw(dt * caveSystem.speedScale /planetList[name][3])
+ui = UiModule.createAndInitialize()
+wf = ui.getWidgetFactory()
+uiroot = ui.getUi()
 
-setUpdateFunction(onUpdate)
+windowContainer = wf.createContainer('multiple', uiroot, ContainerLayout.LayoutFree)
+windowContainer.setPosition(Vector2(0, 0))
+
+sphere = wf.createImage('sphere', windowContainer)
+sphere.setData(loadImage('png/WX_circle_red.png'))
+sphere.setPosition(Vector2(0,0))
+
+####	draw planetary sytem:
+# drawPlanetarySystems(list)
+
+# caveSystem = list['Sun']
+
+# def onUpdate(frame, t, dt):
+	# global caveSystem
+	# planetList = caveSystem.planetObjList
+	# if caveSystem != None:
+		# for name in planetList:
+			# caveSystem.planetRotate(dt * caveSystem.speedScale /planetList[name][2], name)
+			# planetList[name][1].yaw(dt * caveSystem.speedScale /planetList[name][3])
+
+# setUpdateFunction(onUpdate)
+
